650b922e143a5c9d88dffa7d7800883ced3fd885
j-mahapatra <jagannathm.official@gmail.com>
2023-09-03 22:06:40 +0530

    Added CORS config to allow requests from client

--- index.js
+++ index.js
@@ -21,8 +21,14 @@ const app = express();
 // Database connection
 connectToDb(MONGODB_URI);
 
+const corsOptions = {
+  origin: [process.env.CLIENT_URL],
+  credentials: true,
+  exposedHeaders: ['set-cookie'],
+};
+
 // Middlewares
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
